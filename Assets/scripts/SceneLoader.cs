b0f63dc7e701f1c867cc86c6fcd7b6e0b588024b
Memon Shoyeb <memonshoyeb26@gmail.com>
2020-02-05 21:15:01 +0530

    button scene change

--- Assets/scripts/SceneLoader.cs
+++ Assets/scripts/SceneLoader.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadNextScreen()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
